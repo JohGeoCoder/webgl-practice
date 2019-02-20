ab99acac07ffe4686c18bb413b716de0e6de1fa8
John George <john@nepaweb.solutions>
2019-02-20 13:55:06 -0500

    Implementing free object placement webgl app

--- react-webgl/src/components/StoreFrontApp.tsx
+++ react-webgl/src/components/StoreFrontApp.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { MeshObject } from '../models/MeshObject';
 
-import { PerspectiveCamera, Scene, Mesh, WebGLRenderer, BoxGeometry, MeshNormalMaterial } from 'three';
+import { PerspectiveCamera, Scene, Mesh, WebGLRenderer, 
+        BoxGeometry, MeshNormalMaterial, OrthographicCamera,
+        Color, Raycaster, PlaneBufferGeometry, Vector2,
+        MeshBasicMaterial, GridHelper, BoxBufferGeometry } from 'three';
+
+import DragControls from 'three-dragcontrols';
 
 
 interface StoreFrontAppProps {
@@ -14,8 +19,22 @@ export const StoreFrontApp = (props: StoreFrontAppProps) => {
 
     var camera: any, scene: any, renderer: any;
     var geometry: any, material: any, mesh: any;
+    var mouse: any, raycaster: any;
+    var plane: any, rollOverMesh: any;
+
+    var canvasWidth = 1000, canvasHeight = 1000;
+
+    var hoverObject: any = {
+        object: null,
+        originalColor: null,
+    };
+
+    var displayRolloverMesh = false;
+    var rolloverMesh;
 
     var cameraX: any, cameraY: any, cameraZ: any;
+
+    var geometryObjects: MeshObject[] = [];
 
     useEffect(() => {
         init();
@@ -26,46 +45,163 @@ export const StoreFrontApp = (props: StoreFrontAppProps) => {
         //setCamera(new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10));
         var canvasContainer: any = document.getElementById('canvasContainer');
 
-        camera = new PerspectiveCamera( 70, 500 / 500, 0.01, 10 );
+        //camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 10, 10000 );
+        var width = 1000;
+        var height = 1000;
+        camera = new OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 )
 
-        cameraX = 1;
-        cameraY = 1;
-        cameraZ = 1.5;
-
-        //camera.position.z = 1;
+        cameraX = 0;
+        cameraY = 800;
+        cameraZ = 0;
+        camera.position.set( cameraX, cameraY, cameraZ );
+        camera.lookAt( 0, 0, 0 );
 
         scene = new Scene();
+        scene.background = new Color( 0xf0f0f0 );
 
-        geometry = new BoxGeometry( 0.2, 0.2, 0.2 );
-        material = new MeshNormalMaterial();
+        raycaster = new Raycaster();
+        mouse = new Vector2();
 
-        mesh = new Mesh( geometry, material );
-        scene.add( mesh );
+        //Grid
+        var gridHelper = new GridHelper( 1000, 20 );
+        scene.add( gridHelper );
+
+        var planeGeometry = new PlaneBufferGeometry( 1000, 1000 );
+        planeGeometry.rotateX( - Math.PI / 2 );
+        plane = new Mesh( planeGeometry, new MeshBasicMaterial( { visible: false } ) );
+        scene.add( plane );
+
+        var rollOverCube = new BoxBufferGeometry( 50, 50, 50 );
+        var rollOverMaterial = new MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+        rollOverMesh = new Mesh( rollOverCube, rollOverMaterial );
+        scene.add( rollOverMesh );        
 
         renderer = new WebGLRenderer( { antialias: true } );
-        renderer.setSize( 500, 500 );
+
+        setCanvas();
         
         canvasContainer.appendChild(renderer.domElement);
 
-        //document.body.appendChild( renderer.domElement );
+        canvasContainer.addEventListener( 'mousemove', onCanvasMouseMove, false );
+        canvasContainer.addEventListener( 'mousedown', onCanvasMouseDown, false );
+        canvasContainer.addEventListener( 'resize', onWindowResize, false );
+    }
+
+    const onWindowResize = () => {
+        setCanvas();
+    }
+
+    const onCanvasMouseMove = (event: any) => {
+        event.preventDefault();
+        setMouseProjection(event);
+    }
+
+    const onCanvasMouseDown = (event: any) => {
+        event.preventDefault();
+
+        setMouseProjection(event);
+
+        raycaster.setFromCamera( mouse, camera );
+        var geometryIntersects = raycaster.intersectObjects( geometryObjects );
+        if ( geometryIntersects.length > 0 ) {
+            // var geometryIntersect = geometryIntersects[ 0 ];
+            // var voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
+            // voxel.position.copy( geometryIntersect.point ).add( geometryIntersect.face.normal );
+            // voxel.position.divideScalar( 50 ).floor().multiplyScalar( 50 ).addScalar( 25 );
+            // scene.add( voxel );
+            // geometryObjects.push( voxel );
+        }
+        else{
+            //Check if the mouse ray intersects with the plane.
+            var planeIntersects = raycaster.intersectObjects([plane]);
+
+            //If the mouse intersects with the plane, display the ghost cube at the position.
+            if ( planeIntersects.length > 0 ) {
+                var planeIntersect = planeIntersects[ 0 ];
+
+                var cubeGeo = new BoxBufferGeometry( 50,50,50 );
+                var cubeMaterial = new MeshBasicMaterial( { color: 0x00ff00 });
+                var newCube = new Mesh(cubeGeo, cubeMaterial);
+                newCube.position.copy( planeIntersect.point ).add( planeIntersect.face.normal );
+
+                new DragControls([newCube], camera, renderer.domElement);
+
+                scene.add(newCube);
+                geometryObjects.push(newCube);
+            }
+        }
+    }
+
+    const setMouseProjection = (event: any) => {
+        //console.log(event)
+        mouse.set(2 * (event.clientX - event.target.offsetLeft) / canvasWidth - 1, -2 * (event.clientY - event.target.offsetTop) / canvasHeight + 1);
+    }
+
+    const setCanvas = () => {
+        camera.aspect = canvasWidth / canvasHeight;
+        camera.updateProjectionMatrix();
+        //canvasSize = Math.min(window.innerWidth, window.innerHeight);
+        renderer.setSize( canvasWidth, canvasHeight);
     }
 
     const animate = () => {
         requestAnimationFrame( animate );
+				
+        raycaster.setFromCamera( mouse, camera );
+        var geometryIntersects = raycaster.intersectObjects( geometryObjects );
 
-        // mesh.rotation.x += 0.01;
-        // mesh.rotation.y += 0.02;
+        //If the mouse is intersecting an object, highlight the object.
+        if ( geometryIntersects.length > 0 ) {
+            displayRolloverMesh = false;
 
-        cameraX -= 0.01;
-        //cameraY -= 0.01;
-        cameraZ -= 0.01;
+            var geometryIntersect = geometryIntersects[ 0 ];
+            
+            //If the intersected object is newly intersected, update the state.
+            if (geometryIntersect.object !== hoverObject.object){
+
+                //If there was a previously intersected object, reset it's color to its original color.
+                if(hoverObject.object){
+                    hoverObject.object.material.color.setHex(hoverObject.originalColor);
+                }
+                
+                //Store the intersected object and its original color.
+                hoverObject.object = geometryIntersect.object;
+                hoverObject.originalColor = geometryIntersect.object.material.color.getHex();
+
+                //Highlight the intersected object.
+                geometryIntersect.object.material.color.setHex( 0xaa2299 );
+            }
+        }
+        //If the mouse is not intersecting an object, display a ghost cube.
+        else{
+            displayRolloverMesh = true;
+
+            //Reset any previously hovered object.
+            if(hoverObject.object){
+                hoverObject.object.material.color.setHex(hoverObject.originalColor);
+                hoverObject.object = null;
+                hoverObject.originalColor = null;
+            }
+        }
+
+        if(displayRolloverMesh){
+            //Check if the mouse ray intersects with the plane.
+            var planeIntersects = raycaster.intersectObjects([plane]);
+
+            //If the mouse intersects with the plane, display the ghost cube at the position.
+            if ( planeIntersects.length > 0 ) {
+                var planeIntersect = planeIntersects[ 0 ];
+                rollOverMesh.position.copy( planeIntersect.point ).add( planeIntersect.face.normal );
+                rollOverMesh.material.opacity = 0.5;
+            }
+        } else{
+            rollOverMesh.material.opacity = 0.0;
+        }
         
-        camera.position.set( cameraX, cameraY, cameraZ );
-        // camera.position.x = cameraX;
-        // camera.position.y = cameraY;
-        // camera.position.z = cameraZ;
-        camera.lookAt( 0, 0, 0 );
+        render();
+    }
 
+    const render = () => {
         renderer.render( scene, camera );
     }
 
